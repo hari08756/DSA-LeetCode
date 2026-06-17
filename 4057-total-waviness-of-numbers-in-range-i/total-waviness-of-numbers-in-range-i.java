0c3384bd51ceb4c2504b0ec804a222ab372b0bdb
Hariom Maurya <hari08756om@gmail.com>
2026-06-17 11:40:47 +0530

    Time: 18 ms (87.83%) | Memory: 42.5 MB (97.89%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int greater(int a, int b, Set<Integer> unique){
+    public int greater(int a, int b){
         if(a <= 99 && b <= 99) return 0;
         int max = 0;
         while(a > 99 && b > 99){ 
             int x = a % 10, y = (a/10) % 10, z = (a/100)%10;
-            if(((x > y && z > y) || (x<y && z < y)) && !unique.contains(x+y*10+z*100)){
+            if((x > y && z > y) || (x<y && z < y)){
                 max++;
-                // unique.add(x+y*10+z*100);
             }
             a /= 10;
             b /= 10;
@@ -15,10 +14,9 @@ class Solution {
     }
     public int totalWaviness(int num1, int num2) {
         int maxWaviness = 0;
-        Set<Integer> unique = new HashSet<>();
         num1 = Math.max(num1, 101);
         while(num1 <= num2){
-            maxWaviness += greater(num1, num2, unique);
+            maxWaviness += greater(num1, num2);
             num1++;
         }
         return maxWaviness;
